a93cb4bbb5337e126b568ba58cd8d4f6977a7bdc
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-09-18 11:54:24 +0300

    [+] Fixed tests

--- src/test/groovy/org/clematis/skos/parser/RdfReaderTest.groovy
+++ src/test/groovy/org/clematis/skos/parser/RdfReaderTest.groovy
@@ -108,16 +108,5 @@ class RdfReaderTest {
         Assertions.assertEquals(1, paths.size())
         Assertions.assertEquals(4, paths.get(0).size())
     }
-
-    @Test
-    void testCircularRdfBig() {
-        Taxonomy taxonomy = RdfStreamReader.getTaxonomyFromXml("parser/Wiley-Atypon_Taxonomy_Concept_09-08-2024_converted.skos")
-        Assertions.assertEquals(24, taxonomy.getTopConcepts().size())
-
-        List<Stack<Concept>> paths = TaxonomyUtils.getCircularDependencies(taxonomy)
-
-        Assertions.assertEquals(1, paths.size())
-        Assertions.assertEquals(4, paths.get(0).size())
-    }
 }
 
